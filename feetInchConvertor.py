9550b3fcee0f7e4c5e0f389eeabb5e20763a1e6f
tihomirtx88 <tihomirtx88@gmail.com>
2025-07-14 12:01:02 +0300

    Implemenet basic convertor with ne accept inch and feet

---
+++ feetInchConvertor.py
@@ -0,0 +1,27 @@
+import FreeSimpleGUI as sg
+from converters import convert
+
+feet_label = sg.Text("Enter feet: ")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter inches: ")
+inches_input = sg.Input(key="inches")
+
+button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [button, output_label]])
+
+while True:
+    event, values = window.read()
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color="white")
+
+
+window.close()
