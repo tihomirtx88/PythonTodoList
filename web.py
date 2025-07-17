3e9edf26112c80f9c7eec44a00ae1432ee28410c
tihomirtx88 <tihomirtx88@gmail.com>
2025-07-17 14:45:36 +0300

    Complete todo items
    Implemenet  complete funcitonality for todo items

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import session_state
+
 from modules.functions import get_todos, set_todos
 
 todos = get_todos()
@@ -12,6 +14,11 @@ st.title("My Todo App");
 st.subheader("This is my todo appstream");
 
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f"todo_{index}")
+    checkbox = st.checkbox(todo, key=index)
+    if checkbox:
+        todos.pop(index)
+        set_todos(todos)
+        del session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
